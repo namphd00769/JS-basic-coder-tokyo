3d40d36a752c179c2bdd96a18c756ecaba183c4b
namphd00769 <namphd00769@fpt.edu.vn>
2019-03-12 01:49:28 +0700

    Test git checkout -b branch and git checkout branch

---
+++ 19-new-keyword/crush.js
@@ -0,0 +1,18 @@
+function Crush(name, age, address){
+
+}
+
+/**
+ * git checkout -b <branch> tạo ra 1 branch mới nếu branch đó chưa tồn tại , và nhảy vào branch đó làm việc
+ * (vừa tạo mới và vừa checkout)
+ */
+
+/**
+ * git checkout <branch> chuyển sang branch đó làm việc
+ * vd :   git checkout master   ----> nếu cảm thấy đang làm feature crush.js mà bị lỗi , muốn bỏ hẳn file đó đi
+ * ===> thì chỉ việc quay lại branch master bằng cú pháp như trên .
+ *
+ * Note: nếu muốn chuyển sang branch master để làm việc thì trước tiên cần commit branch hiện tại
+ * (feature/crush-class) của file crush.js
+ * 
+ */
