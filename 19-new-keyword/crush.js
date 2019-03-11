bebcd49a718fa95dd42343071458ea8bc8e711ac
namphd00769 <namphd00769@fpt.edu.vn>
2019-03-12 01:55:37 +0700

    retest git checkout branch

--- 19-new-keyword/crush.js
+++ 19-new-keyword/crush.js
@@ -10,7 +10,8 @@ function Crush(name, age, address){
 /**
  * git checkout <branch> chuyển sang branch đó làm việc
  * vd :   git checkout master   ----> nếu cảm thấy đang làm feature crush.js mà bị lỗi , muốn bỏ hẳn file đó đi
- * ===> thì chỉ việc quay lại branch master bằng cú pháp như trên .
+ * ===> thì chỉ việc quay lại branch master bằng cú pháp như trên. Nhưng file đó vẫn hiển thị trên EDITOR
+ * mà chưa mất hẳn , nếu vẫn muốn sửa thì có thể gõ :  git checkout feature/crush-class => chính là tên của branch 
  *
  * Note: nếu muốn chuyển sang branch master để làm việc thì trước tiên cần commit branch hiện tại
  * (feature/crush-class) của file crush.js
