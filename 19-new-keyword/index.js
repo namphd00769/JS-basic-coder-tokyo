8828e0e35484a1a6ff6ed538da78b3ca3d3144a4
namphd00769 <namphd00769@fpt.edu.vn>
2019-03-11 22:09:12 +0700

    add Mouse function contructor

---
+++ 19-new-keyword/index.js
@@ -0,0 +1,32 @@
+//Contructor Function 
+
+function Mouse(name , weight , color){
+	this.type = "mouse";
+	this.name = name;
+	this.weight = weight;
+	this.color = color;
+
+}
+
+var micky = new Mouse( "micky", 0.5, 'red');
+var jerry = new Mouse( "jerry", 0.2, 'yellow');
+
+console.log(micky);
+console.log(jerry);
+
+
+var tom = {
+	name: 'tom',
+	stomach: [],
+	eat: function(mouse){
+		this.stomach.push(mouse);
+		return this; // tra ve this => nen goi duoc nhieu method
+	}
+};
+
+var m1 = new Mouse('m1', 0.1 , 'blue')
+var m2 = new Mouse('m2', 0.5 , 'green')
+var m3 = new Mouse('m3', 0.3 , 'black')
+
+tom.eat(m1).eat(m2).eat(m3);
+console.log(tom);
