d66bd3bfb11ae6f6469ca2899fee1966952832d6
namphd00769 <namphd00769@fpt.edu.vn>
2019-03-12 02:35:09 +0700

    Final git branch and merging branch

--- 19-new-keyword/crush.js
+++ 19-new-keyword/crush.js
@@ -1,6 +1,12 @@
-function Crush(name, age, address){
-
+function Crush(){
+	this.address = [];
 }
+
+Crush.prototype.go = function(country){
+	this.address.push(Japan) ;
+};
+
+
 
 /**
  * git checkout -b <branch> tạo ra 1 branch mới nếu branch đó chưa tồn tại , và nhảy vào branch đó làm việc
@@ -16,4 +22,17 @@ function Crush(name, age, address){
  * Note: nếu muốn chuyển sang branch master để làm việc thì trước tiên cần commit branch hiện tại
  * (feature/crush-class) của file crush.js
  * 
+ */
+
+/**
+ * Merging là: Khi mình đã hài lòng với feature mà vừa làm xong ,  thì mình phảo Merging nó vào branch master
+ * có nghĩa là kéo thay đổi ở branch feature/crush-class vào branch master
+ *
+ * B1: Mình phải checkout sang branch master để làm việc
+ *
+ * B2: Mình sẽ merging branch feature/crush-class vào branch master
+ *
+ * B3: XÓA branch dư thừa (feature/crush-class) : git branch -D feature/crush-class
+ *
+ * ----> Note: Thường là chỉ cần 1 branch master duy nhất còn tồn tại để có thể deyploy lên server
  */
